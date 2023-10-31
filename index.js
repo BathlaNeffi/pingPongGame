d032f8df9b1d613cc5f009928f99970743787a5f
invincible <neffybathla@gmail.com>
2023-10-31 13:29:16 +0530

    added the touch functionality

--- index.js
+++ index.js
@@ -77,19 +77,37 @@ let score,
 
     }
 
-window.addEventListener('keypress', function(){
+
+
+window.addEventListener('keydown', function(){
 
 
 
-    let headSpeed=20;
+    let headSpeed=125;
     let headRect=head1.getBoundingClientRect();
 
-    if(this.event.code=="KeyD" && (headRect.x+head1.offsetWidth <windowWidth)){
-        head1.style.left= (headRect.x) + headSpeed +'px';
-        head2.style.left= head1.style.left;
-    } else if(this.event.code==='KeyA' && (headRect.x > 0)){
-        head1.style.left= (headRect.x) - headSpeed +'px';
-        head2.style.left= head1.style.left;
+    if((this.event.code=="KeyD"|| this.event.code=="ArrowRight") && (headRect.x+head1.offsetWidth <windowWidth)){
+        for(let i=1;i<=headSpeed;i++){
+            increment(i);
+        }
+        function increment(i){
+            setTimeout(function(){
+                head1.style.left= (headRect.x) + i +'px';
+                head2.style.left= head1.style.left;
+            },1*i);
+        }
+        
+       
+    } else if((this.event.code==='KeyA'|| this.event.code=="ArrowLeft") && (headRect.x > 0)){
+        for(let i=1;i<=headSpeed;i++){
+            increment(i);
+        }
+        function increment(i){
+            setTimeout(function(){
+                head1.style.left= (headRect.x) + -i +'px';
+                head2.style.left= head1.style.left;
+            },1*i);
+        }
 
     }
 
@@ -164,5 +182,147 @@ window.addEventListener('keypress', function(){
 
 })
 
+
+document.addEventListener('touchstart', handleTouchStart, false);        
+document.addEventListener('touchmove', handleTouchMove, false);
+
+var xDown = null;                                                        
+var yDown = null;
+
+function getTouches(evt) {
+  return evt.touches ||             // browser API
+         evt.originalEvent.touches; // jQuery
+}                                                     
+                                                                         
+function handleTouchStart(evt) {
+    const firstTouch = getTouches(evt)[0];                                      
+    xDown = firstTouch.clientX;                                      
+    yDown = firstTouch.clientY;                                      
+};                                                
+                                                                         
+function handleTouchMove(evt) {
+
+    let headSpeed=50;
+    let headRect=head1.getBoundingClientRect();
+    if ( ! xDown || ! yDown ) {
+        return;
+    }
+
+    var xUp = evt.touches[0].clientX;                                    
+    var yUp = evt.touches[0].clientY;
+
+    var xDiff = xDown - xUp;
+    var yDiff = yDown - yUp;
+                                                                         
+    if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {/*most significant*/
+        if ( xDiff > 0 ) {
+            /* right swipe */ 
+            // console.log('swipe left');
+
+            if( headRect.x > 0){
+                for(let i=1;i<=headSpeed;i++){
+                    increment(i);
+                }
+                function increment(i){
+                    setTimeout(function(){
+                        head1.style.left= (headRect.x) - i +'px';
+                        head2.style.left= head1.style.left;
+                    },1*i);
+                }
+            }
+        } else {
+            /* left swipe */
+            // console.log('swipe right');
+
+            if( headRect.x+head1.offsetWidth <windowWidth){
+                for(let i=1;i<=headSpeed;i++){
+                    increment(i);
+                }
+                function increment(i){
+                    setTimeout(function(){
+                        head1.style.left= (headRect.x) +i +'px';
+                        head2.style.left= head1.style.left;
+                    },1*i);
+                }
+            }
+        }                       
+    } else {
+        if ( yDiff > 0 ) {
+            /* down swipe */ 
+        } else { 
+            /* up swipe */
+        }                                                                 
+    }
+    /* reset values */
+    xDown = null;
+    yDown = null; 
+    
+    if(evt){
+        if(!gameOn){
+            gameOn=true;
+            beat.play();
+
+
+            let ballRect=ball.getBoundingClientRect();
+            let ballX=ballRect.x;
+            let ballY=ballRect.y;
+            let ballDia=ballRect.width;
+        
+            let head1Width=head1.offsetWidth;
+            let head1Hight=head1.offsetHeight;
+            let head2Width=head2.offsetWidth;
+            let head2Hight=head2.offsetHeight;
+
+            // now moving the ball
+
+            movement=setInterval(function(){
+                ballX+=ballSpeedX;
+                ballY+=ballSpeedY;
+                ball.style.top=ballY +'px';
+                ball.style.left=ballX +'px';
+
+                var head1X=head1.offsetLeft;
+                var head2X=head2.offsetLeft;
+
+                //  reverse direction in case of ball hits the vertical walls
+
+                if((ballX+ballDia) >windowWidth || ballX<0){
+                    ballSpeedX=-ballSpeedX;
+                }
+
+                // It specifies the center of the ball on the viewport
+                let ballPos = ballX + ballDia / 2;
+
+                // checking for rod1 hit
+
+                if(ballY<=head1Hight){
+                    ballSpeedY=-ballSpeedY;
+                    score++;
+                    //  now checking if the ball hits the head or not
+
+                    if((ballPos<head1X)|| ballPos>(head1Width+head1X)){
+                        storeWin(head2Name,score);
+                    }
+                }
+                // now checking for head2
+                else if((ballY + ballDia)>=(windowHeight-head2Hight)){
+                    ballSpeedY=-ballSpeedY;
+                    score++;
+                    //  now checking for if  ball hits the head or not
+                    if((ballPos <head2X)|| ballPos>(head2Width+head2X)){
+                        storeWin(head1Name,score);
+                    }
+                }
+
+
+
+
+
+            },10)
+
+
+        }
+    }
+};
 
 
